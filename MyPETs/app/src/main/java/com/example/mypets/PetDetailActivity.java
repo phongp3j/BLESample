5edd3e4b5620b63a4f039ccc767d0a05440342df
hduc <anojn750@gmail.com>
2024-10-05 15:27:19 +0700

    fix: show pet details

--- MyPETs/app/src/main/java/com/example/mypets/PetDetailActivity.java
+++ MyPETs/app/src/main/java/com/example/mypets/PetDetailActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,7 +14,9 @@ public class PetDetailActivity extends AppCompatActivity {
 
     public static final String KEY_PET_DETAILS_DISPLAY = "key_pet_details";
     private static final String TAG = "PetDetailActivity";
-    private Button btnEditPet;
+
+    private TextView tvPetName, tvPetInfo, tvPetWeight, tvHeartRate, tvTemperature;
+    private Button btnEditPet, btnBack;
 
     private Pet pet;
 
@@ -27,6 +30,8 @@ public class PetDetailActivity extends AppCompatActivity {
 
         initView();
 
+        btnBack.setOnClickListener(v -> onBackPressed());
+
         btnEditPet.setOnClickListener(v -> {
             Log.d(TAG, "onCreate: press btn edit pet " + pet.getId() + " " + pet.getName());
 
@@ -37,6 +42,21 @@ public class PetDetailActivity extends AppCompatActivity {
     }
 
     public void initView() {
+        // find widget
+        tvPetName = findViewById(R.id.tv_pet_name);
+        tvPetInfo = findViewById(R.id.tv_pet_info);
+        tvPetWeight = findViewById(R.id.tv_pet_weight);
+        tvHeartRate = findViewById(R.id.tv_heart_rate);
+        tvTemperature = findViewById(R.id.tv_temperature);
+
+        btnBack = findViewById(R.id.btn_back);
         btnEditPet = findViewById(R.id.btn_edit_pet);
+
+        // set text
+        tvPetName.setText(pet.getName());
+        tvPetInfo.setText(pet.getBreed() + ", " + pet.getAge() + " years");
+        tvPetWeight.setText(String.valueOf(pet.getWeight()) + " kg");
+        tvHeartRate.setText("120 bpm");
+        tvTemperature.setText("101.5 F");
     }
 }
